6124868f59249f6a6e9272d62d42136e91a9ebc2
David <davidabraham@ucla.edu>
2025-04-16 10:14:50 -0700

    updated HS functions and implemented into PR methods; not tested for PR yet

--- src/hashin_shtrikman.jl
+++ src/hashin_shtrikman.jl
@@ -1,4 +1,4 @@
-export Gu, Gl, G1HS, G2HS
+export G1HS, G2HS
 
 # *** Hashin and Shtrikman Bounds ***
 
@@ -34,22 +34,4 @@ function G2HS(K::Float64, c11::Float64, c12::Float64, c44::Float64)::Float64
     a = 5.0 / (g1 - g2)
     b = 6.0 * β2
     return g2 + 2.0 * (a - b)^(-1)
-end
-
-function Cs(c11::Float64, c12::Float64)::Float64
-    return (c11 - c12) / 2.0
-end
-
-function Gu(K::Float64, c11::Float64, c12::Float64, c44::Float64)::Float64
-    cs = Cs(c11, c12)
-    a = 5.0 / (cs - c44)
-    b = (18.0 / 5.0*c44) * (K + 2.0*c44) / (3.0*K + 4.0*c44)
-    return c44 + 2.0*(a + b)^(-1)
-end
-
-function Gl(K::Float64, c11::Float64, c12::Float64, c44::Float64)::Float64
-    cs = Cs(c11, c12)
-    a = 5.0 / (c44 - cs)
-    b = 12.0 * (K + 2.0*cs) / (5.0*cs * (3.0*K + 4.0*cs))
-    return cs + 3.0*(a + b)^(-1)
 end
